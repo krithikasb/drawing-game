9638f456a79c7859fa5d7a306c4c3b82b3e7cb48
Krithika <krithikabq@gmail.com>
2021-11-17 16:48:05 +0530

    add firebase sign-in

--- firebase.js
+++ firebase.js
@@ -9,3 +9,25 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+firebase.auth().signInAnonymously()
+.then(() => {
+  // Signed in..
+})
+.catch((error) => {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    var uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
